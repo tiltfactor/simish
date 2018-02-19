9c11f8a770f7e5aa7c2dc7b44a68b8b4d42a3db6
Gareth Solbeck <gsolbeck@gmail.com>
2018-02-19 17:45:40 -0500

    Use DB-stored vote score in softmatch algorithm
    If the vote score algorithm changes in the future, we will only need to
    change it in the PHP files.

--- domain/InputResponse.go
+++ domain/InputResponse.go
@@ -1,17 +1,5 @@
 package domain
 
-import (
-	"math"
-)
-
-// voteAsymptoteSlope is the slope of the total-vote function. It determines how
-// quickly our confidence increases as we get more votes for a pair.
-const voteAsymptoteSlope float64 = 0.2
-
-// initialVoteScore is the score given to a pair with no votes. As the pair is
-// up-voted or down-voted, it increases or decreases from that point.
-const initialVoteScore float64 = 0.7
-
 // InputOutput used to map the database structure to the input output pair used by the
 // program.
 type InputOutput struct {
@@ -21,8 +9,7 @@ type InputOutput struct {
 	Input      string `json:"pc_input" gorm:"column:pc_input"`
 	Output     string `json:"gm_response" gorm:"column:gm_response"`
 	RoomID     int64  `json:"room_id"`
-	Upvotes	   float64  `gorm:"column:pc_upvotes_weighted"`
-	Downvotes  float64  `gorm:"column:pc_downvotes_weighted"`
+	VoteScore  float64  `gorm:"column:vote_score"`
 }
 
 // InputOutputStore is the interface that needs to be fulfilled by other store implementations.
@@ -57,30 +44,11 @@ func SoftMatch(input string, pairs []InputOutput) (InputOutput, float64) {
 	for _, pair := range pairs {
 		dbTokens := prepareInput(pair.Input)
 		rawScore := getRawScore(userTokens, dbTokens)
-		voteScore := getVoteScore(pair.Upvotes, pair.Downvotes)
-		score := rawScore * voteScore
+		score := rawScore * pair.VoteScore
 		if score > maxScore {
 			maxScore = score
 			response = pair
 		}
 	}
 	return response, maxScore
-}
-
-func getVoteScore(upvotes float64, downvotes float64) float64 {
-	totalVotes := upvotes + downvotes
-	upvoteRatio := upvotes / totalVotes
-	if math.IsNaN(upvoteRatio) {
-		upvoteRatio = 0
-	}
-
-	// totalVoteAsymptote is a value from 0 to 1. When there are no votes, totalAsymptote = 0.
-	// As the number of votes increases, totalAsymptote increases towards 1
-	totalVoteAsymptote := 1 - 1 / (voteAsymptoteSlope * totalVotes + 1)
-
-	// voteScore is a value from 0 to 1. When there are no votes, voteScore = 0.5.
-	// As we get more votes, the voteScore becomes more extreme, increasing towards
-	// 0 if there are more down-votes, or 1 if there are more up-votes
-	voteScore := initialVoteScore + totalVoteAsymptote * (upvoteRatio - initialVoteScore);
-	return voteScore
 }
